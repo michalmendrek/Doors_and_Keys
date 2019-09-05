792e99f7096dcb98dc3c94b758d89d29818efcf5
Michal <michal.mendrek@intive.com>
2019-09-05 15:14:30 +0200

    Key algo improvment

--- key/key.cpp
+++ key/key.cpp
@@ -1,5 +1,13 @@
 #include <key.h>
 
-key::key() {}
+key::key(std::function<void(bool)> ButtonPTR) {}
 
 key::~key() {}
+
+bool key::CheckKeys(int val) { return true; }
+
+void key::SetKeys(int nb, bool val) {}
+
+void key::SetNumOfUsers(int val) {
+  if (val > 0 && val < 5) NumberOfUsers = val;
+}
--- key/key.h
+++ key/key.h
@@ -1,11 +1,23 @@
 #ifndef KEY_H
 #define KEY_H
 
+#include <functional>
+#include <iostream>
+#include <vector>
+
 class key {
  private:
+  bool BigButton;
+  std::vector<bool> Keys;
+  bool CheckKeys(int val);
+  int NumberOfUsers;
+  std::function<void(bool)> CheckButton;
+
  public:
-  key();
+  key(std::function<void(bool)> ButtonPTR);
   ~key();
+  void SetKeys(int nb, bool val);
+  void SetNumOfUsers(int val);
 };
 
 #endif
