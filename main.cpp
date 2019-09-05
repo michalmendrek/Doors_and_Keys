23a668527c9c7b3f47a1630854447b09e8a8568b
Michal <michal.mendrek@intive.com>
2019-09-05 12:58:02 +0200

    Allmost working - still some issue while trying to close A and B simoltaneously

--- main.cpp
+++ main.cpp
@@ -17,4 +17,44 @@ void door_b(bool stan) {
     cout << " Door B Opened" << endl;
 }
 
-int main() { door drzwi(door_a, door_b); }
+int main() {
+  door drzwi(door_a, door_b);
+  drzwi.SetDebug(true);
+  drzwi.SetGame(true);
+  drzwi.SetDoorA_S(true);
+  drzwi.SetDoorB_S(true);
+  int tmp(0);
+  while (1) {
+    cout << "Ustaw Stan Drzwi A" << endl;
+    cin >> tmp;
+    if (tmp == 1) {
+      drzwi.SetDoorA_S(true);
+    } else {
+      drzwi.SetDoorA_S(false);
+    }
+    cout << "Ustaw Stan Drzwi B" << endl;
+    cin >> tmp;
+    if (tmp == 1) {
+      drzwi.SetDoorB_S(true);
+    } else {
+      drzwi.SetDoorB_S(false);
+    }
+    cout << "Ustaw rzadanie dla drzwi A" << endl;
+    cin >> tmp;
+    if (tmp == 1) {
+      drzwi.ManageDoorA(true);
+    } else {
+      drzwi.ManageDoorA(false);
+    }
+
+    cout << "Ustaw rzadanie dla drzwi B" << endl;
+    cin >> tmp;
+    if (tmp == 1) {
+      drzwi.ManageDoorB(true);
+    } else {
+      drzwi.ManageDoorB(false);
+    }
+
+    drzwi.DoorRun();
+  }
+}
